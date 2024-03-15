97f3b13ddfff1c3ff26516429556661d3239acd2
Ben Snaas <ben@wearebit.com>
2024-03-15 15:54:00 +0100

    removed distillation implementation

--- former/transformers.py
+++ former/transformers.py
@@ -18,7 +18,6 @@ class GTransformer(nn.Module):
 
         super().__init__()
 
-        self.depth = depth
         self.num_tokens = num_tokens
         self.token_embedding = nn.Embedding(
             embedding_dim=emb, num_embeddings=num_tokens
@@ -47,7 +46,11 @@ class GTransformer(nn.Module):
 
         self.toprobs = nn.Linear(emb, num_tokens)
 
-    def forward(self, x, return_intermediate=False):
+    def forward(self, x):
+        """
+        :param x: A (batch, sequence length) integer tensor of token indices.
+        :return: predicted log-probability vectors for each token based on the preceding tokens.
+        """
         tokens = self.token_embedding(x)
         b, t, e = tokens.size()
 
@@ -56,47 +59,8 @@ class GTransformer(nn.Module):
         )
         x = tokens + positions
 
-        # Intermediate logits storage
-        intermediate_logits = None
+        x = self.tblocks(x)
 
-        # Determine the 1/3th layer index
-        intermediate_layer_index = self.depth // 3
+        x = self.toprobs(x.view(b * t, e)).view(b, t, self.num_tokens)
 
-        for i, block in enumerate(self.tblocks):
-            x = block(x)
-            if i == intermediate_layer_index:
-                # Capture the intermediate logits
-                intermediate_logits = self.toprobs(x.view(b * t, e)).view(
-                    b, t, self.num_tokens
-                )
-
-        final_logits = self.toprobs(x.view(b * t, e)).view(b, t, self.num_tokens)
-
-        if return_intermediate:
-            return F.log_softmax(final_logits, dim=2), F.log_softmax(
-                intermediate_logits, dim=2
-            )
-        else:
-            return F.log_softmax(final_logits, dim=2)
-
-    def DistillLoss(y_true, y_pred_final, y_pred_intermediate, alpha=0.5, T=1):
-        """
-        y_true: Ground truth labels.
-        y_pred_final: Logits from the final layer.
-        y_pred_intermediate: Logits from the intermediate layer.
-        alpha: Weighting factor for the distillation loss component.
-        T: Temperature for softening probabilities; typically > 1.
-        """
-        # Traditional Cross-Entropy Loss
-        ce_loss = F.cross_entropy(y_pred_intermediate, y_true)
-
-        # Distillation Loss (KL Divergence)
-        soft_pred_final = F.log_softmax(y_pred_final / T, dim=2)
-        soft_pred_intermediate = F.softmax(y_pred_intermediate / T, dim=2)
-        distillation_loss = F.kl_div(
-            soft_pred_intermediate, soft_pred_final.detach(), reduction="batchmean"
-        ) * (T * T)
-
-        # Combined Loss
-        total_loss = alpha * ce_loss + (1 - alpha) * distillation_loss
-        return total_loss
+        return F.log_softmax(x, dim=2)
