05cb87e6dc0511d1baf1fc6b0f5c390ac2705397
Ben Snaas <ben@wearebit.com>
2024-05-27 11:17:53 +0200

    .

--- experiments/two-way.py
+++ experiments/two-way.py
@@ -197,7 +197,7 @@ def go(
     batch_size_by_depth = {
         quarter_depth: 260,
         2 * quarter_depth: 135,
-        3 * quarter_depth: 90,
+        3 * quarter_depth: 95,
         depth: 65
     }
 
